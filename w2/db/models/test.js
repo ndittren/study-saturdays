f5fd7a364b973ed7d31863577bb370c030944e7c
Natalia Dittren <natalia.dittren@gmail.com>
2018-08-11 11:59:05 -0400

    update

--- w2/db/models/test.js
+++ w2/db/models/test.js
@@ -1,5 +1,20 @@
 'use strict';
 const Sequelize = require('sequelize');
 const db = require('../db');
+const Student = require('./student')
+
+const Test = db.define('test', {
+  subject: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  grade: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+})
+
+Test.belongsTo(Student);
 
 module.exports = Test;
